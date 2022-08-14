4d4a3bbfe497ddcdbae244efc20e03ce6cba5bc3
Nicolas Lapenna <lapennanic@gmail.com>
2022-08-14 16:22:05 -0400

    reworked baseline pred

--- src/modules/functions.py
+++ src/modules/functions.py
@@ -11,6 +11,8 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 import string
 
+import sklearn.metrics as metrics
+
 # text packages
 from nltk.tokenize import word_tokenize
 
@@ -272,3 +274,17 @@ def text_preprocessing(df):
     preprocessed_df = pd.DataFrame(preprocessed_data)
     return preprocessed_df
 
+def regression_results(y_true, y_pred):
+    # Regression metrics
+    explained_variance=metrics.explained_variance_score(y_true, y_pred)
+    mean_absolute_error=metrics.mean_absolute_error(y_true, y_pred) 
+    mse=metrics.mean_squared_error(y_true, y_pred) 
+    mean_squared_log_error=metrics.mean_squared_log_error(y_true, y_pred)
+    median_absolute_error=metrics.median_absolute_error(y_true, y_pred)
+    r2=metrics.r2_score(y_true, y_pred)
+    print('explained_variance: ', round(explained_variance,4))    
+    print('mean_squared_log_error: ', round(mean_squared_log_error,4))
+    print('r2: ', round(r2,4))
+    print('MAE: ', round(mean_absolute_error,4))
+    print('MSE: ', round(mse,4))
+    print('RMSE: ', round(np.sqrt(mse),4))
